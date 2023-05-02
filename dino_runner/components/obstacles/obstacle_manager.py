253b921b2fb3ef2b1bccfbb940f1993af9b963cc
Gavbr1el <Juan.Aviles@fundacion-jala.org>
2023-05-02 11:27:35 -0400

    Clase 3 - Tarea
    LARGE_CACTUS agregados con su posicion modificada en Y

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,7 @@
+import random
 import pygame
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 
 
 class ObstacleManager():
@@ -9,8 +10,11 @@ class ObstacleManager():
     
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
-
+            aux = random.randint(0,1)
+            if aux == 0:
+                self.obstacles.append(Cactus(LARGE_CACTUS))
+            elif aux == 1:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
